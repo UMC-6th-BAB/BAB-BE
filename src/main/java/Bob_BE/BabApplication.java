5e5b4669518329edd784d5a545002bfbe170918d
hsuush <dlhyes703@naver.com>
2024-07-16 22:35:59 +0900

    setting : 기본구조 세팅

---
+++ src/main/java/Bob_BE/BabApplication.java
@@ -0,0 +1,13 @@
+package Bob_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabApplication.class, args);
+	}
+
+}
