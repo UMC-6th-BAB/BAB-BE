d2fbad4c37fd2ef4119971c591e51d077bb33a1c
wonjun0120 <wonjundero@gmail.com>
2024-07-25 21:33:11 +0900

    fix: @Transaction 어노테이션 추가
    #25

--- src/main/java/Bob_BE/domain/owner/service/OwnerService.java
+++ src/main/java/Bob_BE/domain/owner/service/OwnerService.java
@@ -14,6 +14,7 @@ import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -25,6 +26,7 @@ public class OwnerService {
     private final JwtTokenProvider jwtTokenProvider;
     private final KakaoUserClient kakaoUserClient;
 
+    @Transactional
     public OwnerResponseDto.LoginOrRegisterDto registerOrLogin(OwnerRequestDto.LoginOrRegisterDto request) {
         try {
             String authorization = "Bearer " + request.getToken();
--- src/main/java/Bob_BE/domain/student/service/StudentService.java
+++ src/main/java/Bob_BE/domain/student/service/StudentService.java
@@ -14,6 +14,7 @@ import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -25,6 +26,7 @@ public class StudentService {
     private final JwtTokenProvider jwtTokenProvider;
     private final KakaoUserClient kakaoUserClient;
 
+    @Transactional
     public StudentResponseDto.LoginOrRegisterDto registerOrLogin(StudentRequestDto.LoginOrRegisterDto request) {
         try {
             String authorization = "Bearer " + request.getToken();
