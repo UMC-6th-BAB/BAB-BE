108848eec1cbbf91d2c3624863ea95c44fe33336
Cycrypto <jh01love00@gmail.com>
2024-07-25 18:42:33 +0900

    feat: swagger 문서 추가

--- src/main/java/Bob_BE/domain/store/controller/StoreController.java
+++ src/main/java/Bob_BE/domain/store/controller/StoreController.java
@@ -4,6 +4,11 @@ import Bob_BE.domain.menu.dto.request.MenuRequestDto.MenuCreateRequestDto;
 import Bob_BE.domain.menu.dto.response.MenuResponseDto.CreateMenuResponseDto;
 import Bob_BE.domain.store.service.StoreService;
 import Bob_BE.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,9 +24,17 @@ public class StoreController {
     private final StoreService storeService;
 
     @PostMapping("/{storeId}/menus")
+    @Operation(summary = "메뉴 추가 API", description = "가게에 새로운 메뉴들을 추가하는 API입니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "성공입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE 404", description = "해당하는 가게가 존재하지않습니다.")
+    })
+    @Parameters({
+            @Parameter(name = "storeId", description = "가게 식별자, PathVariable")
+    })
     public ApiResponse<List<CreateMenuResponseDto>> createMenus(
             @PathVariable Long storeId,
-            @RequestBody MenuCreateRequestDto requestDto
+            @RequestBody @Valid MenuCreateRequestDto requestDto
     ){
         var response = storeService.createMenus(storeId, requestDto);
         return ApiResponse.onSuccess(response);
