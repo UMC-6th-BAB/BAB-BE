874fefb42870843672f787b522585d7b9f385f56
wonjun0120 <wonjundero@gmail.com>
2024-08-11 21:49:02 +0900

    fix: 카카오 소셜 로그인 수정
    인가코드 받아올 수 있도록 수정
    
    #90

--- src/main/java/Bob_BE/domain/owner/service/OwnerService.java
+++ src/main/java/Bob_BE/domain/owner/service/OwnerService.java
@@ -7,6 +7,7 @@ import Bob_BE.domain.owner.entity.Owner;
 import Bob_BE.domain.owner.repository.OwnerRepository;
 import Bob_BE.domain.store.entity.Store;
 import Bob_BE.domain.store.repository.StoreRepository;
+import Bob_BE.global.external.KakaoAuthClient;
 import Bob_BE.global.external.KakaoResponseDto;
 import Bob_BE.global.external.KakaoUserClient;
 import Bob_BE.global.response.code.resultCode.ErrorStatus;
@@ -17,6 +18,7 @@ import Bob_BE.global.util.JwtTokenProvider;
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,12 +32,31 @@ public class OwnerService {
     private final OwnerRepository ownerRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final KakaoUserClient kakaoUserClient;
+    private final KakaoAuthClient kakaoAuthClient;
     private final StoreRepository storeRepository;
+
+    @Value("${social.client.kakao.rest-api-key}")
+    private String kakaoAppKey;
+    @Value("${social.client.kakao.secret-key}")
+    private String kakaoAppSecret;
+    @Value("${social.client.kakao.redirect-uri}")
+    private String kakaoRedirectUri;
+    @Value("${social.client.kakao.grant_type}")
+    private String kakaoGrantType;
 
     @Transactional
     public OwnerResponseDto.LoginOrRegisterDto registerOrLogin(OwnerRequestDto.LoginOrRegisterDto request) {
         try {
-            String authorization = "Bearer " + request.getToken();
+            String authorizationCode = request.getToken();
+            KakaoResponseDto.KakaoTokenResponseDto kakaoTokenInfo = kakaoAuthClient.getAccessToken(
+                    kakaoAppKey,
+                    kakaoAppSecret,
+                    kakaoGrantType,
+                    kakaoRedirectUri,
+                    authorizationCode
+            );
+
+            String authorization = "Bearer " + kakaoTokenInfo.getAccessToken();
             KakaoResponseDto.KakaoUserResponseDto kakaoUserInfo = kakaoUserClient.getUserInfo(authorization);
 
             Long socialId = kakaoUserInfo.getId();
