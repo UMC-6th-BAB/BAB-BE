30b7c3eac29e1254e47ef61f5ad963082e4f597c
Isee_ <112475555+iseevict@users.noreply.github.com>
2024-08-23 01:50:39 +0900

    Update StoreService.java

--- src/main/java/Bob_BE/domain/store/service/StoreService.java
+++ src/main/java/Bob_BE/domain/store/service/StoreService.java
@@ -272,7 +272,7 @@ public class StoreService {
             if (ownerStore.isEmpty()) {
 
                 // 만약 사장님이 가게가 없을 경우 default 로 숭실대를 보여주는 것으로 설정했습니다.
-                findUniversity = universityRepository.findById(13L)
+                findUniversity = universityRepository.findById(2L)
                         .orElseThrow(() -> new UniversityHandler(ErrorStatus.UNIVERSITY_NOT_FOUND));
             }
             else {
